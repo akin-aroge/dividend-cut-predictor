ae1a9d6f58a2f7aed52c6ab10fc9ee2bcb6cc635
Akindele F. Aroge <aroogzbaba1@gmail.com>
2023-09-29 17:40:12 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dividend-cut-predictor",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Dividend cut predictor",
+    author="Akin Aroge <arogeakindele@gmail.com>",
+    license="MIT",
+)
